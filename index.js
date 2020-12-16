ce7f798c651b488d8647acfd1a43cbc7ab3f9bb2
Amelie Le Coz <lecozamelie@hotmail.com>
2020-12-16 20:54:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const axios = require("axios");
 
 const getRepos = async(user) => {
     const repoListUrl = `https://api.github.com/users/${user}/repos`;
-    const repos = await axios.get(repoListUrl, { headers: { Accept: "application/json" }})
-    return repos;
+    const response = await axios.get(repoListUrl, { headers: { Accept: "application/json" }})
+    return response.data;
 } 
 
-const getLanguages = repos => {
+const getLanguages = (user, repos) => {
     let languages = [];
-    repos.foreach(async(repo) => {
+    repos.forEach(async(repo) => {
         const repoLanguageUrl = `https://api.github.com/repos/${user}/${repo.name}/languages`;
-        const language = await axios.get(repoLanguageUrl, { headers: { Accept: "application/json" }})
-        languages.push(language);
+        const response = await axios.get(repoLanguageUrl, { headers: { Accept: "application/json" }})
+        languages.push(response.data);
     });
     return languages;
 }
@@ -38,21 +38,25 @@ const sumLanguages = languages => {
 
 process.argv.slice(2).forEach(async(user) => {
     
+    let result = {};
     try{
         // //Récupère la liste des repos
         const repos = await getRepos(user);
+        console.log('repos :', repos);
         // //Pour chaque repo, on récupère les infos de languages
-        const languages = await getLanguages(repos);
+        const languages = await getLanguages(user, repos);
+        console.log('languages :', languages);
         // //On résume les languages dans un seul objet result
-        const result = await sumLanguages(languages);
+        result = await sumLanguages(languages);
         //Log l'output
-        console.log(`Language repartition for ${user}`);
-        Object.keys(result).forEach(language => {
-            console.log(`* ${result[language]}: ${language}`);
-        });
+        
     } catch(err) {
         console.log(err);
     }
+    console.log(`Language repartition for ${user}`);
+        Object.keys(result).forEach(language => {
+            console.log(`* ${result[language]}: ${language}`);
+        });
     
 });
 
